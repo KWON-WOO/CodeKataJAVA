61c9bbfb4f709b9f9d345163eb7dfef8d9beb26f
KWON-WOO <50099560+KWON-WOO@users.noreply.github.com>
2025-10-30 20:36:23 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] temp;
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            temp = new int[commands[i][1]-commands[i][0] + 1];
+            int k = 0;
+            for (int j = commands[i][0] -1; j < commands[i][1]; j++){
+                temp[k++] = array[j];
+            }
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
