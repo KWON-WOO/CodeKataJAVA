bbff69c0d5059f129747ebe55faceb5f1d1a3b12
KWON-WOO <50099560+KWON-WOO@users.noreply.github.com>
2025-11-05 10:24:45 +0900

    [level 1] Title: 콜라 문제, Time: 0.33 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = returnValue(n,a,b,0);
+        return answer;
+    }
+    
+    //result = 반환받은 콜라 수
+    public int returnValue(int num, int a, int b, int result) {
+        int r = (num / a) * b;
+        if (r != 0) {
+            result = returnValue((num % a) + r, a , b, result + r);
+        }
+        return result;
+    }
+}
+//n은 빈병 수
+//a : b 비율로 변환.
+//result는 반환받은 콜라 수
