1b91e212a2ac7883207ddd258948c6a213611f17
KWON-WOO <50099560+KWON-WOO@users.noreply.github.com>
2025-11-08 22:33:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int c1 = 0;
+        int c2 = 0;
+        for (String goalStr: goal) {
+            if (c1 < cards1.length && goalStr.equals(cards1[c1])) c1++;
+            else if (c2<cards2.length && goalStr.equals(cards2[c2])) c2++;
+            else return "No";
+        }
+        return "Yes";
+    }
+}
